05658ae8519e9c25e7daa8dc08cc8f3dc321a0b6
choul111 <54615322+choul111@users.noreply.github.com>
2020-07-10 19:04:27 +0900

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,75 @@
+console.log("비디오 로드");
+
+var video = document.querySelector(".video01");
+var video2 = document.querySelector(".video010");
+video2.play();
+video.play();
+//var nn = 0;
+/*
+var video = document.querySelector(".video01");
+var video2 = document.querySelector(".video010");
+video2.pause();
+video.pause();
+
+var stopN = 3;
+function onScrollWindow(e){
+    var heightY = window.pageYOffset;
+    var durationThis = 2;
+    if(heightY >= 6612 && nn == 0){
+        video.style.opacity = 1;
+        setTimeout(function(){stopN = 0;},6000);
+        playvideo();
+        
+    }
+    
+}
+function playvideo(){
+    var stopThiss = setInterval(function(){
+        video.play();
+        
+        //console.log(stopN);
+        if(stopN == 0){
+            clearInterval(stopThiss);
+            video.pause();
+            nn = 1;
+            console.log("ddd"); 
+        }
+    },2000);
+}
+//console.log(video.currentTime);
+window.addEventListener("scroll", onScrollWindow);
+
+
+
+
+video2.pause();
+var dd = 0;
+var stopNN = 3;
+function onScrollWindowvideo010(e){
+    var heightY = window.pageYOffset;
+    var durationThis = 2;
+    console.log(heightY);
+    if(heightY >= 5369 && dd == 0){
+        video2.style.opacity = 1;
+        setTimeout(function(){stopNN = 0;},6000);
+        playvideo11();
+        
+    }
+    
+}
+function playvideo11(){
+    var stopThiss = setInterval(function(){
+        video2.play();
+        
+        //console.log(stopN);
+        if(stopNN == 0){
+            clearInterval(stopThiss);
+            video2.pause();
+            dd = 1;
+            console.log("ddd"); 
+        }
+    },2000);
+}
+//console.log(video.currentTime);
+window.addEventListener("scroll", onScrollWindowvideo010);
+*/
